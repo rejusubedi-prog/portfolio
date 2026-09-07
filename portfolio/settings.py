6540a0757609ccd5248d98bea3bf07d24a7b9407
rejusubedi-prog <rejusubedi@gmail.com>
2026-09-07 10:19:06 +0545

    remove windows specific dependencies

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'whitenoise',
     'home',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
